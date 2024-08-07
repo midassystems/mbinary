894b89e8376f89480dd392e4964fa4dcb2a39343
anthonyb8 <anthonybaxter819@gmail.com>
2024-08-07 09:08:55 -0400

    UPD: Updated python_mbn with BID_ASK pair.

--- mbn/src/python/records.rs
+++ mbn/src/python/records.rs
@@ -1,39 +1,65 @@
-use crate::records::{Mbp1Msg, OhlcvMsg, RecordHeader};
+use std::os::raw::c_char;
+
+use crate::records::{BidAskPair, Mbp1Msg, OhlcvMsg, RecordHeader};
 use pyo3::prelude::*;
 
 // TODO: Add BIDAsK PAIR
 
-// #[pymethods]
-// impl Mbp1Msg {
-//     #[new]
-//     fn py_new(
-//         hd: RecordHeader,
-//         price: i64,
-//         size: u32,
-//         action: u16,
-//         side: u16,
-//         flags: u8,
-//         ts_recv: u64,
-//         ts_in_delta: i32,
-//         sequence: u32,
-//     ) -> Self {
-//         Mbp1Msg {
-//             hd,
-//             price,
-//             size,
-//             action,
-//             side,
-//             flags,
-//             ts_recv,
-//             ts_in_delta,
-//             sequence,
-//         }
-//     }
+#[pymethods]
+impl BidAskPair {
+    #[new]
+    fn py_new(
+        bid_px: i64,
+        ask_px: i64,
+        bid_sz: u32,
+        ask_sz: u32,
+        bid_ct: u32,
+        ask_ct: u32,
+    ) -> Self {
+        BidAskPair {
+            bid_px,
+            ask_px,
+            bid_sz,
+            ask_sz,
+            bid_ct,
+            ask_ct,
+        }
+    }
+}
 
-//     fn __str__(&self) -> String {
-//         format!("{:?}", self)
-//     }
-// }
+#[pymethods]
+impl Mbp1Msg {
+    #[new]
+    fn py_new(
+        hd: RecordHeader,
+        price: i64,
+        size: u32,
+        action: c_char,
+        side: c_char,
+        depth: u8,
+        ts_recv: u64,
+        ts_in_delta: i32,
+        sequence: u32,
+        levels: [BidAskPair; 1],
+    ) -> Self {
+        Mbp1Msg {
+            hd,
+            price,
+            size,
+            action,
+            side,
+            depth,
+            ts_recv,
+            ts_in_delta,
+            sequence,
+            levels,
+        }
+    }
+
+    fn __str__(&self) -> String {
+        format!("{:?}", self)
+    }
+}
 
 #[pymethods]
 impl OhlcvMsg {
