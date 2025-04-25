afb6b3daed0686ce46aa1072fbbe048f7f32810a
anthonyb8 <anthonybaxter819@gmail.com>
2025-04-25 15:49:06 -0400

    Fix imported location for libmbinary.a

--- scripts/build_c.sh
+++ scripts/build_c.sh
@@ -7,7 +7,6 @@ mkdir -p "$BASE_DIR/include"
 if cd "$BASE_DIR/c"; then
 	if cargo build --release; then
 		cp "$BASE_DIR/target/mbinary.h" "$BASE_DIR/include/mbinary.h"
-		# cp "$BASE_DIR/target/release/libmbinary.a" "$BASE_DIR/../mbinary-build/libmbinary.a"
 		echo "mbinary compiled."
 	else
 		echo "ERROR: cargo build failed"
